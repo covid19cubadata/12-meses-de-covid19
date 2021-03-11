576ab20b83206a853a001e639bd7c65e6259faf3
cbermudez <cbermudez.dev@gmail.com>
2021-03-10 23:03:18 -0500

    Use c3js

--- js/sex-pie.js
+++ js/sex-pie.js
@@ -1,28 +1,19 @@
-var maleCases = 30102;
-var femaleCases = 28277;
-var casesColors = [
-    '#B01E22',
-    '#1C1340'
-];
+var maleCases = ['Hombres', 30102];
+var femaleCases = ['Mujeres', 28277];
+var casesColors = {
+    'Hombres': '#B01E22',
+    'Mujeres': '#1C1340'
+};
 
 function buildSexsPie() {
-    var sexPieCtx = document.getElementById('sexPie').getContext('2d');
-    console.log(sexPieCtx)
-    var myPieChart = new Chart(sexPieCtx, {
-        type: 'pie',
+    // var sexPieCtx = document.getElementById('sexPie').getContext('2d');
+    var sexPie = c3.generate({
+        bindto: '#sexPie',
         data: {
-            datasets: [{
-                data: [maleCases, femaleCases],
-                backgroundColor: casesColors,
-            }],
-            labels: [
-                'Hombres',
-                'Mujeres'
-            ],
+            type: 'pie',
+            columns: [maleCases, femaleCases],
+            colors: casesColors
         },
-        options: {
-            responsive: true
-        }
     });
 };
 
