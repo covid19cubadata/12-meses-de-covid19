4d702c8a69ab4172cea14299b8aaa12782c15f8e
cbermudez <cbermudez.dev@gmail.com>
2021-03-10 22:38:38 -0500

    Add male and female pie chart

---
+++ js/sex-pie.js
@@ -0,0 +1,30 @@
+var maleCases = 30102;
+var femaleCases = 28277;
+var casesColors = [
+    '#B01E22',
+    '#1C1340'
+];
+
+function buildSexsPie() {
+    var sexPieCtx = document.getElementById('sexPie').getContext('2d');
+    console.log(sexPieCtx)
+    var myPieChart = new Chart(sexPieCtx, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: [maleCases, femaleCases],
+
+            }],
+            backgroundColor: casesColors,
+            labels: [
+                'Hombres',
+                'Mujeres'
+            ],
+        },
+        options: {
+            responsive: true
+        }
+    });
+};
+
+buildSexsPie();
